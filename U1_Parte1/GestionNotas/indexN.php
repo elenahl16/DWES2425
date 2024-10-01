ed17ef589a891f02a1a3165e3c128206f17531a1
Elena Hernández <ehernandezl17@educarex.es>
2024-10-01 19:50:18 +0200

    lo que llevo

---
+++ U1_Parte1/GestionNotas/indexN.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion notas</title>
+</head>
+<body>
+    <h3>Gestion de Notas y Exámenes</h3>
+    
+<form action="#" method="post" enctype="multipart/form-data"> 
+<div>
+    <label for="fecha_nota">Fecha creacion Notas</label>
+    <input type="date" name="fecha_nota" id="fecha_nota" 
+ 
+</div>
+
+</form>
+
+</body>
+</html>
