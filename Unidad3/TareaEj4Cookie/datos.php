c8f35e726f5dc9414a8352a18079d7344be7cffa
Elena Hernández <ehernandezl17@educarex.es>
2024-10-14 01:13:04 +0200

    ejercicio casa

---
+++ Unidad3/TareaEj4Cookie/datos.php
@@ -0,0 +1,73 @@
+<?php
+class Datos{
+
+    private $fecha,$hora,$asunto;
+
+    function __construct() {
+        
+    }
+
+
+
+    /**
+     * Get the value of fecha
+     */ 
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set the value of fecha
+     *
+     * @return  self
+     */ 
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hora
+     */ 
+    public function getHora()
+    {
+        return $this->hora;
+    }
+
+    /**
+     * Set the value of hora
+     *
+     * @return  self
+     */ 
+    public function setHora($hora)
+    {
+        $this->hora = $hora;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of asunto
+     */ 
+    public function getAsunto()
+    {
+        return $this->asunto;
+    }
+
+    /**
+     * Set the value of asunto
+     *
+     * @return  self
+     */ 
+    public function setAsunto($asunto)
+    {
+        $this->asunto = $asunto;
+
+        return $this;
+    }
+}
+?>
+
---
+++ Unidad3/TareaEj4Cookie/ejer4.php
@@ -0,0 +1,53 @@
+<?php
+require_once "datos.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <h1>Calendario de Eventos</h1>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>
+                    Fecha
+                    <br>
+                    <input type="date" name="fecha" id="fecha"/></br>
+                </td>
+                
+                <td>
+                    Hora
+                    <br>
+                    <input type="time" name="hora" id="hora"/></br>   
+                </td>
+                <td>
+                    Asunto
+                    <br>
+                    <input type="text" name="asunto" id="asunto" placeholder="Asunto"/></br>
+                </td>
+                </tr>
+        </table>
+        <input type="submit" name="crear" value="Añadir"/>
+        <input type="submit" name="borrar" value="Borrar"/>
+    </form>
+</body>
+
+<?php
+//validamos que se han introducido datos
+    if(isset($_POST["crear"])){
+        //si estan rellano los datos
+        if(!empty($_POST['fecha']) || !empty($_POST['hora']) || !empty($_POST['fecha'])){
+            $datosEvent= new Datos();
+
+        }
+
+    }else{
+        echo "No puede a ver ningun campo vacio";
+    }
+
+?>
+</html>
