c7a9d4a4b2082a7e88ff29c67549fd7d83c30320
Elena Hernández <ehernandezl17@educarex.es>
2024-11-18 14:28:51 +0100

    actualizado la unidad4

---
+++ Unidad4/prueba/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('holaMundo');
+});
+
+Route::get('/alumnos', function () {
+    return 'Bienvenidos Alumnos';
+});
+
+Route::get('/alumnos/{nombre}', function ($nombreA) {
+    //aqui estoy definiendo una ruta con el nombre 
+    return 'Bienvenido ' .$nombreA;
+});
+
+Route::get('/alumnos/insertar/{nombre}', function ($nombreA) {
+    //aqui estoy definiendo una ruta con el nombre 
+    return 'Página para crear alumno ' .$nombreA;
+});
